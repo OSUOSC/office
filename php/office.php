5165da8cc9ed503dd97d388704790a04e1627ed4
bsilvereagle <bsilvereagle@gmail.com>
2014-04-24 14:03:20 -0400

    Made PHP near equivalent to meinwalds

--- php/office.php
+++ php/office.php
@@ -5,18 +5,52 @@ include 'PhpSerial.php';
 
 header('Content-type: text/plain');
 
-echo "1";
-
 $serial = new PhpSerial;
-echo "serial created";
 $serial->deviceSet("/dev/ttyACM1");
 $serial->confBaudRate(9600);
 $serial->deviceOpen();
-echo "Opened";
 
+/* O will cause the Arduino to respond with
+   1 or 0 corresponding to yes/no*/
 $serial->sendMessage("O");
 
-echo $serial->readPort();
+$loggedon = $serial->readPort();
 
 $serial->deviceClose();
+
+if(array_key_exists('html',$_REQUEST))
+{
+	$html = true;
+	if($loggedon)
+	{
+		echo "<html><head><title>Come join the party!</title></head>\n";
+	}
+	else
+	{
+		echo "<html><head><title>Nobody here...</title></head>\n";
+	}
+	echo "<body><pre>";
+}
+else
+{
+	$html = false;
+	header('Content-type: text/plain');
+}
+if($loggedon)
+{
+	echo " /-----\\\n";
+	echo " | \\o/ |\n";
+	echo "There is probably someone in the office.\n";
+}
+else
+{
+	echo " /-----\\\n";
+	echo " |     |\n";
+	echo "There is likely nobody in the office.\n";
+}
+if($html)
+{
+        echo "</pre></body></html>";
+}
+
 ?>
